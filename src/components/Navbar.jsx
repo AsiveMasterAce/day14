74e8986bda59adbc74f6dea1d05dec4615224065
AsiveMasterAce <asiveacemathambeka@gmail.com>
2024-03-14 22:26:01 +0200

    Link changes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -11,10 +11,11 @@ const Navbar = () => {
  return (
     <nav className="bg-white bg-opacity-60 dark:bg-gray-900 fixed w-full z-20 top-0 start-0 ">
       <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-        <a href="" className="flex items-center space-x-3 rtl:space-x-reverse">
+        {/* meant to be link */}
+        <div className="flex items-center space-x-3 rtl:space-x-reverse">
           <img src={logo} className="h-10" alt="My Drop Logo" />
           {/* <span className="self-center text-2xl font-semibold whitespace-nowrap dark:bg-pink-700">My Drop</span> */}
-        </a>
+        </div>
         <div className="flex md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
           <button type="button" className="text-white bg-pink-700 hover:bg-pink-400 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Get started</button>
           <button onClick={toggleNavbar} type="button" className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-sticky" aria-expanded="false">
@@ -26,10 +27,10 @@ const Navbar = () => {
         </div>
         <div className={`items-center justify-between hidden w-full md:flex md:w-auto md:order-1 ${isOpen ? 'block' : 'hidden'}`} id="navbar-sticky">
           <ul className="flex flex-col p-4 md:p-0 mt-4 font-medium border md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0  dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
-            <li>
-              <a href="" className="block py-2 px-3 text-white bg-pink-700 rounded md:bg-transparent md:text-pink-700 md:p-0 md:dark:text-pink-500" aria-current="page">Home</a>
-            </li>
 {/* 
+            <li>
+              <a href="/home" className="block py-2 px-3 text-white bg-pink-700 rounded md:bg-transparent md:text-pink-700 md:p-0 md:dark:text-pink-500" aria-current="page">Home</a>
+            </li>
             <li>
               <a href="#" className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-pink-400 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">About</a>
             </li>
